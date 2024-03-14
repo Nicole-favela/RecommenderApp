cfd35873321b8a438cbecf2b0d7d10dce65ebce2
Nicole Favela <nicolefavela@Nicoles-MacBook-Pro-2.local>
2024-03-14 15:23:59 -0600

    added backend url

--- client/src/config/urls.js
+++ client/src/config/urls.js
@@ -1,5 +1,6 @@
-const BASE_URL = 'http://localhost:5000';
+const DEV_URL = 'http://localhost:5000';
+const BASE_URL = 'https://movierecommenderapp.onrender.com';
 const POSTER_URL = 'https://image.tmdb.org/t/p/original/';
 
 
-export {BASE_URL, POSTER_URL}
+export { BASE_URL, POSTER_URL }
